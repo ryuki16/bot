1e782f6da2a6595fdd1a11b2ca0582d072e7b3c5
Kaisan <muhammad.kaisanaryana@gmail.com>
2026-07-05 18:16:24 +0800

    Add basic Discord bot with commands

---
+++ bot_class.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def halo(ctx):
+    await ctx.send(f'Hi!')
+
+@bot.command()
+async def bye(ctx):
+    await ctx.send("\U0001f642")
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+bot.run("token")
